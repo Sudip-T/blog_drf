b6ed3f7baa144266a5e9b09595fdcf20d6105da5
Sudip-T <myselfsudipthapa1994@gmail.com>
2024-06-20 11:17:48 +0545

    api documentaion : swagger added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# blog_drf
+# DRF Blog App
+
+A simple Django blog application with CRUD functionality for blog posts.
+
+## Installation
+
+1. Clone the repository:
+   git clone https://github.com/your-username/django-blog.git
+
+2. Install dependencies:
+   pip install -r requirements.txt
+
+3. 
--- blog/urls.py
+++ blog/urls.py
@@ -14,15 +14,34 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from drf_yasg import openapi
 from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
+from django.urls import path, include
+from rest_framework import permissions
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blog API",
+        default_version='v1',
+        description="APIs provided by Test Blog",
+        terms_of_service="https://www.episyche.com/terms/",
+        contact=openapi.Contact(email="contact@episyche.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('accounts.urls')),
     path('api/v1/', include('blog_app.urls')),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 # Conditionally serve media files only in DEBUG mode
